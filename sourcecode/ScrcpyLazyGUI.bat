43e378b6ff33682516d00e53379ea3121083c693
jotesoft <jotesoftus@gmail.com>
2025-02-06 16:06:46 +0600

    run using bat

---
+++ sourcecode/ScrcpyLazyGUI.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWH3eyFEzOhhEVUmVNWXsUvgKu6aq0/mEsE4NTd0zeZXCmoupFckv40bre9gk1XU6
+::fBE1pAF6MU+EWH3eyFEzOhhEVUmVNWXsUvgKu6aq0/mEsE4NTd0zeZXCmoupFckv2Ub2doY0+ntAgew5JVVQahPL
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdCuDJEqH5kEgMVZDRQrUaCasVftN1Pv4/emXu3IRVfgmOLD8752yKeMc5AvtdplN
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBNQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZkIaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAl3MbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIDKwlXXB3CEGqgH/U73cG7zfPHiEIJUfEwfpu7
+::eg0/rx1wNQPfEVWB+kM9LVsJDDeBLmiqH5kd8vHc2sPn
+::fBEirQZwNQPfEVWB+kM9LVsJDDeBLmiqH5kd8vHc2sPn
+::cRolqwZ3JBvQF1fEqQIDKwlXXB3CEGqgH/U73cG77PiCo1kYUKI9Yc/R1aCLM+8V/iU=
+::dhA7uBVwLU+EWHOL4EcjJx1ALA==
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEphJifXs=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJEqH5kEgMVZDRQrUaCasVftN1Pv4/emXu3IRVfgmOLD8752yE+MB6VX9WZc3z11vsYUJFB44
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+start "" "ScrcpyLazyGUI.hta"
